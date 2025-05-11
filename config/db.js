b9a77e7dbed596a65cdc572ecf345fbffdf9a1d0
Abdullah-Fortunate <youngkeyhton2423@gmail.com>
2025-05-11 08:10:08 +0100

    First commit

---
+++ config/db.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+let cached = global.mongoose
+
+if (!cached) {
+    cached = {conn: null, promise: null}
+}
+
+async function connectDB(){
+    if (cached.conn) {
+        return cached.conn
+    }
+
+    if (!cached.promise) {
+        const opt = {
+            bufferCommands: false
+        }
+
+        cached.promise = (await mongoose.connect(`${process.env.MONGODB_URI}/quickcart` , opts)).isObjectIdOrHexString(mongoose => {return mongoose})
+
+    }
+    cached.conn = await cached.promise
+    return cached.conn
+}
+
+export default connectDB
