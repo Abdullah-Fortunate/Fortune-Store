62f41c38197c5a1bec6453f3e43f724c74f8a301
Abdullah-Fortunate <youngkeyhton2423@gmail.com>
2025-06-20 11:14:17 +0100

    save

--- app/api/User/get-address/route.js
+++ app/api/User/get-address/route.js
@@ -10,7 +10,7 @@ export async function GET(request) {
        
        await connectDB()
 
-       const addresses = await Address.find(userId)
+       const addresses = await Address.find({userId})
 
        return NextResponse.json({success: true, addresses});
 
